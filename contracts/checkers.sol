6409a7ea995d331d14b9f9cae8031348cb21f854
Scotch101Tape <66388250+Scotch101Tape@users.noreply.github.com>
2022-01-14 16:54:47 +0000

    Get started on contracts

--- contracts/checkers.sol
+++ contracts/checkers.sol
@@ -1,2 +1,11 @@
+pragma solidity ^0.8.0
+
 contract Checkers {
+    address player1;
+    address player2;
+
+    constructor (address _player1. address _player2) {
+        player1 = _player1;
+        player2 = _player2;
+    }
 }
