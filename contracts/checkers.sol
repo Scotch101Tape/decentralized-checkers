46ddb03036550319fe11fdf974919581dc0bee6a
Scotch101Tape <66388250+Scotch101Tape@users.noreply.github.com>
2022-01-15 00:00:47 +0000

    Add waffle

--- contracts/checkers.sol
+++ contracts/checkers.sol
@@ -1,10 +1,12 @@
-pragma solidity ^0.8.0
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
 
 contract Checkers {
     address player1;
     address player2;
 
-    constructor (address _player1. address _player2) {
+    constructor (address _player1, address _player2) {
         player1 = _player1;
         player2 = _player2;
     }
