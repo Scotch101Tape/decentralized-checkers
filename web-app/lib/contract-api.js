a21768c7a6f840ee6aed41971725eb907cc73755
Scotch101Tape <scotch101tape@gmail.com>
2022-02-17 17:07:03 -0600

    Add PieceEnum to contract-api

--- web-app/lib/contract-api.js
+++ web-app/lib/contract-api.js
@@ -1,11 +1,19 @@
 import { ethers } from "ethers";
-import { Board, PieceEnum } from "./board"
+import { Board } from "./board"
 
 const DECENTRALIZED_CHECKERS_ADDRESS = "0x874C1c696F62F71Aa76D2e4812776c0771956017"
 
 export const PlayerEnum = {
   Me: 1,
   Other: 2,
+}
+
+export const PieceEnum = {
+  None: 0,
+  Black: 1,
+  White: 2,
+  BlackKing: 3,
+  WhiteKing: 4
 }
 
 export class DecentralizedCheckersEnv {
