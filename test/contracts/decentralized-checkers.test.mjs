00ce19b95664b8c790166782903876165bc5f86c
Scotch101Tape <scotch101tape@gmail.com>
2022-02-22 11:44:56 -0600

    Update decentralized-checkers testing suite

--- test/contracts/decentralized-checkers.test.mjs
+++ test/contracts/decentralized-checkers.test.mjs
@@ -2,6 +2,7 @@ import {assert, expect, use} from 'chai';
 import {Contract} from 'ethers';
 import {deployContract, MockProvider, solidity} from 'ethereum-waffle';
 import DecentralizedCheckers from "../../build/DecentralizedCheckers.json";
+import {Player} from "../../contracts/checkers.constants.mjs"
 
 use(solidity);
 
@@ -15,10 +16,16 @@ describe("decentralized-checkers", () => {
     decentralizedCheckers = await deployContract(deployer, DecentralizedCheckers, []);
   });
 
-  it("Create a game", async () => {
+  it("Emits OnGameStart", async () => {
     let player1Contract = await decentralizedCheckers.connect(player1);
     let player2Contract = await decentralizedCheckers.connect(player2);
-    await player1Contract.joinPublicGame();
-    expect(await player2Contract.joinPublicGame()).to.emit(decentralizedCheckers, "gameStarted")
+    await player1Contract.joinGame()
+    expect(await player2Contract.joinGame()).to.emit(decentralizedCheckers, "OnGameStart")
   });
+
+  it("Allows cancelation", async () => {
+    let player1Contract = await decentralizedCheckers.connect(player1);
+    await player1Contract.joinGame();
+    await player1Contract.cancelJoinGame();
+  })
 })
